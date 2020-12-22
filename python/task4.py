aa1f47e50f7a891631d9e44d9437cd74c906066d
kannab98 <ponur0kirill@yandex.ru>
2020-12-23 00:52:23 +0300

    add pics

--- python/task4.py
+++ python/task4.py
@@ -1,10 +1,18 @@
 from automator import LabAutomator
 import numpy as np
 import matplotlib.pyplot as plt
+import os
+import subprocess
+import time
+
+
+current_dir = os.getcwd()
+app = subprocess.Popen(os.path.join(current_dir, "Match_filter.exe"))
+time.sleep(5)
 
 lab = LabAutomator()
 
-lab.select_lfv()
+lab.select_lfm()
 lab.noise_on()
 lab.set_noise(4)
 lab.set_frequency(1000)
@@ -17,9 +25,11 @@ N = 10
 step = 5
 tau = np.arange(10, 100, 5)
 
-for t in tay:
+
+for t in tau:
     lab.set_duration(t)
     for i in range(N):
         lab.build()
         lab.save_screenshot("task4/t4s4_%.0f_%s.png" % (t, i))
+        lab.save_output("task4/t4s4_%.0f_%s_out.png" % (t, i))
         lab.clean()
