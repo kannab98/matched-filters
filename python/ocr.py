31ebd69f363c31f4a33844204c6127a35c5ec561
Kirill Ponur <ponur0kirill@gmail.com>
2020-12-23 01:29:39 +0300

    прикрутил распозновалку

--- python/ocr.py
+++ python/ocr.py
@@ -7,36 +7,46 @@ import pandas as pd
 import numpy as np
 
 
-rootdir = "."
-regex = re.compile('.*.png')
+rootdir = "task4"
+regex = re.compile('.*out.png')
 
 U = [[]]
 
-# for root, dirs, files in os.walk(rootdir):
-#   for file in files:
-#     if regex.match(file):
+listfiles = []
+for root, dirs, files in os.walk(rootdir):
+  for file in files:
+    if regex.match(file):
+        listfiles.append(os.path.join(rootdir,file))
 
-img = cv2.imread('test.png')
-img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-gray = cv2.resize(img, None, fx=2, fy=2, interpolation=cv2.INTER_CUBIC)
-img = cv2.merge([gray,gray,gray])
+listfiles = np.sort(listfiles)
+# listfiles = listfiles[0:10]
 
-img = img[:,0:img.shape[0]//2,:]
-# plt.imshow(img)
-# plt.show()
+U = np.full((len(listfiles), 3), None)
 
-cfg = "digits"
-# # Распознавание, допустимы только цифры
-digits = pytesseract.run_and_get_output(img, extension='txt', config=cfg).split()
-for i, digit in enumerate(digits):
-    if digit[0] == '0':
-        digits[i] = digit[0] + '.' + digit[1:]
+for j, file in enumerate(listfiles):
+    img = cv2.imread(file)
 
-    U[-1].append(float(digits[i]))
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    gray = cv2.resize(img, None, fx=2, fy=2, interpolation=cv2.INTER_CUBIC)
+    img = cv2.merge([gray,gray,gray])
+
+    img = img[:,0:img.shape[0]//2,:]
+
+
+    cfg = "digits"
+    # # Распознавание, допустимы только цифры
+    digits = pytesseract.run_and_get_output(img, extension='txt', config=cfg).split()
+
+    if digits[0][0] == '0':
+        digits[0] = digits[0][0] + '.' + digits[0][1:]
+
+    print(digits)
+    for i, digit in enumerate(digits):
+        U[j,i] = digits[i]
 
 U = np.array(U).T
-
 df = pd.DataFrame({'in': U[0], 'out': U[1], 'out_mean': U[2]})
+print(df)
 df.to_excel('task4.xlsx', index=False)
 
 
--- python/task4_plot.py
+++ python/task4_plot.py
@@ -4,9 +4,12 @@ import numpy as np
 
 
 df = pd.read_excel('task4.xlsx', engine='openpyxl')
+print(df)
 
 fig, ax = plt.subplots()
 ax.plot(df["out"])
 
 fig, ax = plt.subplots()
 ax.plot(df["out_mean"])
+
+plt.show()
